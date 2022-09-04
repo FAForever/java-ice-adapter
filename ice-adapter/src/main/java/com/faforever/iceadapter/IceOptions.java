06b3530c18f6fe48e6ac5ccb5691ad9b00d63a7c
Brutus5000 <Brutus5000@gmx.net>
2022-09-04 10:23:21 +0200

    Use PicoCLI over custom args parsing

---
+++ ice-adapter/src/main/java/com/faforever/iceadapter/IceOptions.java
@@ -0,0 +1,45 @@
+package com.faforever.iceadapter;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import picocli.CommandLine;
+import picocli.CommandLine.Option;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class IceOptions {
+    @Option(names = "--id", required = true, description = "set the ID of the local player")
+    private int id;
+
+    @Option(names = "--login", required = true, description = "set the login of the local player e.g. \"Rhiza\"")
+    private String login;
+
+    @Option(names = "--rpc-port", defaultValue = "7236", description = "set the port of internal JSON-RPC server")
+    private int rpcPort;
+
+    @Option(names = "--gpgnet-port", defaultValue = "0", description = "set the port of internal GPGNet server")
+    private int gpgnetPort;
+
+    @Option(names = "--lobby-port", defaultValue = "0", description = "set the port the game lobby should use for incoming UDP packets from the PeerRelay")
+    private int lobbyPort;
+
+    @Option(names = "--force-relay", description = "force the usage of relay candidates only")
+    private boolean forceRelay;
+
+    @Option(names = "--debug-window", description = "activate the debug window")
+    private boolean debugWindow;
+
+    @Option(names = "--info-window", description = "activate the info window")
+    private boolean infoWindow;
+
+    @Option(names = "--delay-ui", defaultValue = "0", description = "delays the launch of the info and debug window (in ms)")
+    private int delayUi;
+
+    @Option(names = "--ping-count", defaultValue = "1", description = "number of times to ping each turn server to determine latency")
+    private int pingCount;
+
+    @Option(names = "--acceptable-latency", defaultValue = "250.0", description = "number of times to ping each turn server to determine latency")
+    private double acceptableLatency;
+}
