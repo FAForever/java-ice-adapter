77e190c2f429077aad948c0b2fd020aa83508cae
Brutus5000 <Brutus5000@gmx.net>
2022-10-02 22:18:08 +0200

    Add button to info window to open telemetry web ui

--- ice-adapter/src/main/java/com/faforever/iceadapter/debug/InfoWindowController.java
+++ ice-adapter/src/main/java/com/faforever/iceadapter/debug/InfoWindowController.java
@@ -1,45 +1,73 @@
 package com.faforever.iceadapter.debug;
 
+import com.faforever.iceadapter.IceAdapter;
 import com.faforever.iceadapter.util.TrayIcon;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import lombok.SneakyThrows;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.util.concurrent.ExecutorService;
 
 public class InfoWindowController {
-	public Button killAdapterButton;
-	public Button showDebugWindowButton;
-	public Button minimizeToTray;
+    public Button killAdapterButton;
+    public Button showDebugWindowButton;
+    public Button showTelemetryWebUiButton;
+    public Button minimizeToTray;
 
 
-	private volatile int killRequests = 0;
-	public void onKillAdapterClicked(ActionEvent actionEvent) {
-		killRequests++;
+    private volatile int killRequests = 0;
 
-		if(killRequests < 3) {
-			killAdapterButton.setText("This will disconnect you from the game! Click " + (3 - killRequests) + " more times.");
-		} else {
-			System.exit(337);
-		}
-	}
+    public void onKillAdapterClicked(ActionEvent actionEvent) {
+        killRequests++;
 
-	public void onShowDebugWindowClicked(ActionEvent actionEvent) {
-		DebugWindow.INSTANCE.thenAcceptAsync(DebugWindow::showWindow);
-	}
+        if (killRequests < 3) {
+            killAdapterButton.setText("This will disconnect you from the game! Click " + (3 - killRequests) + " more times.");
+        } else {
+            System.exit(337);
+        }
+    }
 
-	public void onMinimizeToTrayClicked(ActionEvent actionEvent) {
-		if (TrayIcon.isTrayIconSupported() && InfoWindow.INSTANCE != null) {
-			InfoWindow.INSTANCE.minimize();
-		}
-	}
+    public void onShowDebugWindowClicked(ActionEvent actionEvent) {
+        DebugWindow.INSTANCE.thenAcceptAsync(DebugWindow::showWindow);
+    }
 
-	@FXML
-	private void initialize() {
-		killAdapterButton.setOnAction(this::onKillAdapterClicked);
-		showDebugWindowButton.setOnAction(this::onShowDebugWindowClicked);
-		minimizeToTray.setOnAction(this::onMinimizeToTrayClicked);
+    @SneakyThrows
+    public void onTelemetryWebUiClicked(ActionEvent actionEvent) {
+        String url = "%s/app.html?gameId=%d&playerId=%d".formatted(
+                IceAdapter.TELEMETRY_SERVER.replaceFirst("ws", "http"),
+                IceAdapter.gameId,
+                IceAdapter.id
+        );
 
-		if (!TrayIcon.isTrayIconSupported()) {
-			minimizeToTray.setVisible(false);
-		}
-	}
+        new Thread(() -> {
+            try {
+                Desktop.getDesktop().browse(URI.create(url));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }).start();
+    }
+
+    public void onMinimizeToTrayClicked(ActionEvent actionEvent) {
+        if (TrayIcon.isTrayIconSupported() && InfoWindow.INSTANCE != null) {
+            InfoWindow.INSTANCE.minimize();
+        }
+    }
+
+    @FXML
+    private void initialize() {
+        killAdapterButton.setOnAction(this::onKillAdapterClicked);
+        showDebugWindowButton.setOnAction(this::onShowDebugWindowClicked);
+        showTelemetryWebUiButton.setOnAction(this::onTelemetryWebUiClicked);
+        minimizeToTray.setOnAction(this::onMinimizeToTrayClicked);
+
+        if (!TrayIcon.isTrayIconSupported()) {
+            minimizeToTray.setVisible(false);
+        }
+    }
 }
