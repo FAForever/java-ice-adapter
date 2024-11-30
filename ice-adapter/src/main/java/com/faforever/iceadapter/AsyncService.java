ce67d04371c8a0c33c4e6d43e8e608a43cf2924c
Super <Super4121@gmail.com>
2024-12-01 03:17:18 +0600

    AsyncService optimization

--- ice-adapter/src/main/java/com/faforever/iceadapter/AsyncService.java
+++ ice-adapter/src/main/java/com/faforever/iceadapter/AsyncService.java
@@ -1,13 +1,11 @@
 package com.faforever.iceadapter;
 
-import com.faforever.iceadapter.exception.AsyncException;
 import lombok.experimental.UtilityClass;
 
-import java.util.concurrent.Callable;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.locks.Lock;
+import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
@@ -17,27 +15,13 @@ public class AsyncService {
     private final ExecutorService executor = Executors.newVirtualThreadPerTaskExecutor();
 
     public CompletableFuture<Void> runAsync(Runnable task) {
-        return callAsync(() -> {
-            task.run();
-            return null;
-        });
+        return CompletableFuture.runAsync(task, executor);
     }
 
     public CompletableFuture<Void> runAsync(Runnable task, String nameThread) {
-        return callAsync(() -> {
+        return runAsync(() -> {
             Thread.currentThread().setName(nameThread);
             task.run();
-            return null;
-        });
-    }
-
-    public <T> CompletableFuture<T> callAsync(Callable<T> task) {
-        return supplyAsync(() -> {
-            try {
-                return task.call();
-            } catch (Exception e) {
-                throw new AsyncException(e);
-            }
         });
     }
 
@@ -58,28 +42,11 @@ public class AsyncService {
     }
 
     public void executeDelayed(int timeMs, Runnable runnable) {
-        runAsync(() -> {
-            try {
-                if (timeMs > 0) {
-                    Thread.sleep(timeMs);
-                }
-                runnable.run();
-            } catch (InterruptedException e) {
-                throw new AsyncException(e);
-            }
-        });
-    }
-
-    public void executeWithLock(Lock lock, Runnable task) {
-        lock.lock();
-        try {
-            task.run();
-        } finally {
-            lock.unlock();
-        }
+        CompletableFuture.runAsync(runnable, CompletableFuture.delayedExecutor(timeMs, TimeUnit.MILLISECONDS, executor));
     }
 
     public void close() {
         executor.shutdown();
+        CompletableFuture.runAsync(executor::shutdownNow, CompletableFuture.delayedExecutor(1000, TimeUnit.MILLISECONDS));
     }
 }
